87a5d62478291c881a67764e37ff79e75e95853e
Tammy Lam <lamtammy1515@gmail.com>
2020-04-12 20:55:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end 
   
-   get '/:operation/:number1/:number2' do
+  get '/:operation/:number1/:number2' do
     operation = params[:operation]
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
