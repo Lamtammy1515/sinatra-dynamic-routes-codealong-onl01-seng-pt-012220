1177d19209d55c6c3830ef441dbbd5a9c8bf3429
Tammy Lam <lamtammy1515@gmail.com>
2020-04-12 20:45:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,9 +22,9 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do 
     @num1 = params[:num1.to_i]
     @num2 = params[:num2.to_i]
-    when "multiply"
-      (@num1 * @num2).to_s
-    end
+    @operation = params[:operation]
+
+    @num1.send(@operation,@num2)
   end
 
 end
